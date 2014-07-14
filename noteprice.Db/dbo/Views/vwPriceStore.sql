5c10943da783d1e29f6dd20f4aaffd12f3b477df
mitimer <mikhailtimofeev@gmail.com>
2014-07-14 15:53:20 +0400

    new views added to db project

---
+++ noteprice.Db/dbo/Views/vwPriceStore.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vwPriceStore]
+AS
+select 	
+	p.Id as PriceId,
+	p.[Text] as PriceText,
+	s.Id as StoreId,
+	s.Name as StoreName,
+	s.Location as StroeLocation,
+	s.StoreSetId as StoreSetId,
+	ss.Name as StoreSetName
+from [dbo].Price p	
+	left join 
+	[dbo].Store s
+		on p.SotreId = s.Id
+		left join 
+		[dbo].[StoreSet] ss
+			on s.StoreSetId = ss.Id
---
+++ noteprice.Db/dbo/Views/vwStoreStoreSet.sql
@@ -0,0 +1,14 @@
+﻿
+CREATE VIEW [dbo].[vwStoreStoreSet]
+AS
+select 	
+	s.Id as StoreId,
+	s.Name as StoreName,
+	s.Location as StoreLocation,
+	s.StoreSetId as StoreSetId,
+	ss.Name as StoreSetName
+from 
+	[dbo].Store s
+		left join 
+		[dbo].[StoreSet] ss
+			on s.StoreSetId = ss.Id
